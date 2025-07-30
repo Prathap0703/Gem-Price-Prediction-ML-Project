ffd1244bf43f53df64f4025b06b6042cee19cea7
Prathap0703 <vprathap2004@gmail.com>
2025-07-30 22:22:06 +0530

    Setup

--- setup.py
+++ setup.py
@@ -1,32 +1,25 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-HYPHEN_E_DOT = '-e .'
+HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
-    '''
-    This function will return list of requirements
-    from requirements.txt file
-    '''
-    # Initializing blank requirements list
-    requirements = []
-
-    # Opening the file
+    requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
-        
-        # Remove hyphen_e_dot if present in requirements
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
     return requirements
 
+
 setup(
-    name = 'ML_regression',
-    version= '0.0.1',
+    name='GEMPricePrediction',
+    version='0.0.1',
     author='Prathap',
     author_email='vprathap0703@gmail.com',
-    packages = find_packages(),
-    install_requires = get_requirements('requirements.txt')
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
 )
