7db229823b5072c54161cb9ab8c2e163dbafd396
Prathap0703 <vprathap2004@gmail.com>
2025-07-30 22:22:34 +0530

    Application

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, render_template,jsonify
+from flask_cors import cross_origin
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+application = Flask(__name__)
+
+app = application
+
+@app.route('/')
+@cross_origin()
+def home_page():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+@cross_origin()
+def predict_datapoint():
+    if request.method == 'POST':
+        try:
+            data = CustomData(
+                carat = float(request.form.get('carat')),
+                depth = float(request.form.get('depth')),
+                table = float(request.form.get('table')),
+                x = float(request.form.get('x')),
+                y = float(request.form.get('y')),
+                z = float(request.form.get('z')),
+                cut = request.form.get('cut'),
+                color = request.form.get('color'),
+                clarity = request.form.get('clarity')
+            )
+
+            pred_df = data.get_data_as_dataframe()
+            print("DataFrame for prediction:\n", pred_df)
+
+            predict_pipeline = PredictPipeline()
+            pred = predict_pipeline.predict(pred_df)
+            print("Prediction:", pred)
+
+            results = round(pred[0], 2)
+            return render_template('index.html', results=results)
+
+        except Exception as e:
+            print("Internal Error:", str(e))  # prints full error to terminal
+            return f"<h3>Internal Error: {str(e)}</h3>"  
+    else:
+        return render_template('index.html')
+
+    
+@app.route('/predictAPI',methods=['POST'])
+@cross_origin()
+def predict_api():
+    if request.method=='POST':
+        data = CustomData(
+            carat = float(request.json['carat']),
+            depth = float(request.json['depth']),
+            table = float(request.json['table']),
+            x = float(request.json['x']),
+            y = float(request.json['y']),
+            z = float(request.json['z']),
+            cut = request.json['cut'],
+            color = request.json['color'],
+            clarity = request.json['clarity']
+        )
+
+        pred_df = data.get_data_as_dataframe()
+        predict_pipeline = PredictPipeline()
+        pred = predict_pipeline.predict(pred_df)
+        dct = {'price':round(pred[0],2)}
+        return jsonify(dct)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000,debug=True)
